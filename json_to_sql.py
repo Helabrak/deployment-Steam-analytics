1f84e9401ea53ec83b725a9c585691c29b295bb0
Quinten <84379913+QuintenMM@users.noreply.github.com>
2021-08-27 14:02:06 +0200

    parsing and putting data into the sql database

---
+++ json_to_sql.py
@@ -0,0 +1,32 @@
+import json
+import sqlite3
+
+import pandas as pd
+import json
+from sqlalchemy import create_engine
+
+# read file
+
+with open("steam scrape/database.json") as f:
+    data =json.load(f)
+data
+df=pd.DataFrame(data)
+df = df.T
+# df = df.drop('website', axis=1)
+print(df.columns)
+
+save_df = df[df['is_free']==1]
+# print(save_df.shape)
+save_df = save_df.applymap(str)
+conn = sqlite3.connect("steam_games.db")
+c = conn.cursor()
+
+save_df.to_sql("steam_games",conn)
+
+sqlite3 steam_games.db
+.schema steam_games;
+. schema steam_games
+select * from steam_games;
+
+
+
